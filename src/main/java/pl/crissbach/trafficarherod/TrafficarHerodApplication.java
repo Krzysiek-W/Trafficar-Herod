f5479fb4ef60f65b4762a45d0bdd25bbdae5ea2b
Krzysztof <k.wiesebach@gmail.com>
2024-11-25 18:11:32 +0100

    Initial project

---
+++ src/main/java/pl/crissbach/trafficarherod/TrafficarHerodApplication.java
@@ -0,0 +1,13 @@
+package pl.crissbach.trafficarherod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficarHerodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficarHerodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/crissbach/trafficarherod/TrafficarHerodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.crissbach.trafficarherod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficarHerodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
